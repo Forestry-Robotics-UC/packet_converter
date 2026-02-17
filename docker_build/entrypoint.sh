a8be69c89d19224a48dcf30a51e93dd7164b2c37
Toby <tobias.hulland@gmail.com>
2026-02-17 19:16:38 +0000

    rqt_bag viewer

--- docker_build/entrypoint.sh
+++ docker_build/entrypoint.sh
@@ -2,10 +2,5 @@
 set -e
 # Source ROS2
 source /opt/ros/$ROS_DISTRO/setup.bash
-# Source the docker container workspace if it exists
-if [ -d /docker_ws/src ]; then
-    cd /docker_ws
-    source /docker_ws/install/setup.bash
-fi
 
 exec "$@"
